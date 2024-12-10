c2c45882d1456a92aa1896762def42ab4b1119a6
Sricharan <renish1419@gmail.com>
2024-12-11 02:06:03 +0530

    lib1

--- src/components/ChartDisplay.tsx
+++ src/components/ChartDisplay.tsx
@@ -6,29 +6,40 @@ import {
   PointElement,
   LineElement,
   BarElement,
+  BarController,
+  PieController,
+  LineController,
+  ScatterController,
+  RadarController,
+  BarController,
+  BubbleController, 
+  ArcElement,
+  RadialLinearScale,
   Title,
   Tooltip,
   Legend,
-  ArcElement,
-  RadialLinearScale,
-  Filler,
-  ChartType
+  Colors
 } from 'chart.js';
-import { Chart } from 'react-chartjs-2';
 
-// Register Chart.js components
 ChartJS.register(
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
   BarElement,
+  BarController,
+  PieController,
+  LineController,
+  ScatterController,
+  RadarController,
+  BarController,
+  BubbleController, 
+  ArcElement,
+  RadialLinearScale,
   Title,
   Tooltip,
   Legend,
-  ArcElement,
-  RadialLinearScale,
-  Filler
+  Colors
 );
 
 interface ChartDisplayProps {
