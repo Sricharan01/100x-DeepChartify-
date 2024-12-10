4804198228c418eb8d403835dc8cc12b581dbe8e
Sricharan <renish1419@gmail.com>
2024-12-11 02:08:30 +0530

    lib3

--- src/components/FileUpload/FileUploadZone.tsx
+++ src/components/FileUpload/FileUploadZone.tsx
@@ -3,6 +3,50 @@ import { useDropzone } from 'react-dropzone';
 import { Upload, AlertCircle } from 'lucide-react';
 import { FileWithPreview } from '../../types/fileTypes';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  BarController,
+  PieController,
+  LineController,
+  ScatterController,
+  RadarController,
+  BarController,
+  BubbleController, 
+  ArcElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  Colors
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  BarController,
+  PieController,
+  LineController,
+  ScatterController,
+  RadarController,
+  BarController,
+  BubbleController, 
+  ArcElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+  Legend,
+  Colors
+);
+
+
 interface FileUploadZoneProps {
   onFilesAccepted: (files: FileWithPreview[]) => void;
 }
